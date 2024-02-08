7af1ed28b09ae68f556bbfd464b8b9c4505046d0
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2024-02-08 00:58:25 +0000

    Update 1.java

--- Variables_and_conditions/1.java
+++ Variables_and_conditions/1.java
@@ -5,11 +5,13 @@ class equalities{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
+    //user input
     System.out.println("Input the first number: ");
     int num1 = input.nextInt();
     System.out.println("Input the second number: ");
     int num2 = input.nextInt();
 
+    //Determines which number is bigger or if the numbers are equal
     if(num1 > num2){
       System.out.println("The first number is bigger");
     }
